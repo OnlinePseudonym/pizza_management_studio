6fd7950d7508f6480d7885b5072b491795484419
Kevin T Reynolds <Kevin.T.Reynolds30@gmail.com>
2019-05-26 19:42:12 -0700

    build login page to authenticate user

---
+++ pms-fe/src/LoginPage/LoginPage.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import { userService } from '../_services/user.service';
+import Loading from '../_components/_images/loading';
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    userService.logout();
+
+    this.state = {
+      username: '',
+      password: '',
+      submitted: false,
+      loading: false,
+      error: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({ submitted: true });
+    const { username, password } = this.state;
+
+    if (!(username && password)) {
+      return;
+    }
+
+    this.setState({ loading: true });
+    userService
+      .login(username, password)
+      .then(user => console.log(user), error => this.setState({ error, loading: false }));
+  }
+
+  render() {
+    const { username, password, submitted, loading, error } = this.state;
+    if (error) {
+      console.log(error);
+    }
+
+    return (
+      <div>
+        <div>
+          Username: {username}
+          <br />
+          Password: {password}
+        </div>
+        <h2>Login</h2>
+        <form name="login-form" onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" value={username} onChange={this.handleChange} />
+            {submitted && !username && <div>Username is required</div>}
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" value={password} onChange={this.handleChange} />
+            {submitted && !password && <div>Password is required</div>}
+          </div>
+          <div>
+            <button disabled={loading}>Login</button>
+            {loading && <Loading />}
+          </div>
+          {error && <div>{error}</div>}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
---
+++ pms-fe/src/_services/user.service.js
@@ -0,0 +1,41 @@
+export const userService = { login, logout };
+
+async function login(email, password) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password })
+  };
+
+  const response = await fetch('http://localhost:4000/api/users/sign_in', requestOptions);
+  const user = await handleResponse(response);
+
+  if (user) {
+    user.authdata = window.btoa(`${email}:${password}`);
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  return user;
+}
+
+function logout() {
+  localStorage.removeItem('user');
+}
+
+function handleResponse(response) {
+  return response.text().then(text => {
+    const data = text && JSON.parse(text);
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        logout();
+        //window.location.assign('/');
+      }
+
+      const error = (data && data.errors.detail) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
